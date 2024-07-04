f84b656868003388f3d86414ca3957790f57e399
a-m-i-t-a-v-a <140612284+a-m-i-t-a-v-a@users.noreply.github.com>
2024-07-04 20:12:03 +0530

    connecting to the db

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+//import dotenv from 'dotenv';
+//dotenv.config({ path: './../../config.env' });
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+const connectDB=async()=>{
+    try{
+        console.log(process.env.DATABASE)
+        const connectionInstance=await mongoose.connect(`${process.env.DATABASE}/${DB_NAME}`)
+        console.log(`\n Mongodb connected!! DB host : ${connectionInstance.connection}`)
+    }catch(err){
+        console.log('Mongodb connection error',err)
+        process.exit(1)
+    }
+}
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,1 +1,110 @@
-console.log('Pillu n Don')
+//require('dotenv').config({path:'./../config.env'})
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./../config.env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from 'express'
+
+const app=express()
+
+;(async()=>{
+    try{
+        await mongoose.connect(`${process.env.DATABASE}/${DB_NAME}`)
+        app.on("error",(err)=>{
+            console.log('Application not able to connect',err)
+            throw err
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is running on port ${process.env.PORT}`)
+        })
+    }catch(err){
+        console.error(`Error received ${err}`)
+    }
+})()*/
